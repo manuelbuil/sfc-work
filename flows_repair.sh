c470e2f912e7da4d5fb652758dac65fdb5526ce1
Manuel Buil <manuel.buil@ericsson.com>
2016-06-13 17:26:54 +0200

    Update flows_repair.sh

--- flows_repair.sh
+++ flows_repair.sh
@@ -10,5 +10,5 @@ echo $output_port
 echo $sf_port
 
 ovs-ofctl -O Openflow13 del-flows br-int "table=11,tcp,reg0=0x1,tp_dst=80"
-ovs-ofctl -O Openflow13 add-flow br-int "table=11,tcp,reg0=0x1,tp_dst=80,priority=5 actions=move:NXM_NX_TUN_ID[0..31]->NXM_NX_NSH_C2[],set_field:0x$ip->nshc1,set_field:$nsp->nsp,set_field:255->nsi,load:0xc0a80003->NXM_NX_TUN_IPV4_DST[],load:0x97->NXM_NX_TUN_ID[0..31],resubmit($output_port,0)"
-ovs-ofctl -O Openflow13 add-flow br-int "table=0,priority=405,udp,in_port=$sf_port,tp_dst=6633 actions=LOCAL"
+ovs-ofctl -O Openflow13 add-flow br-int "table=11,tcp,reg0=0x1,tp_dst=80,priority=5 actions=move:NXM_NX_TUN_ID[0..31]->NXM_NX_NSH_C2[],set_field:0xc0a80022->nshc1,set_field:$nsp->nsp,set_field:255->nsi,load:0xc0a80003->NXM_NX_TUN_IPV4_DST[],load:0x97->NXM_NX_TUN_ID[0..31],resubmit($output_port,0)"
+
