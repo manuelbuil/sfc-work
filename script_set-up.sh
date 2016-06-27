fe04a26020f64ed729c81f45fead95341989bf22
Manuel Buil <manuel.buil@ericsson.com>
2016-06-27 12:29:36 +0200

    Update script_set-up.sh

--- script_set-up.sh
+++ script_set-up.sh
@@ -138,9 +138,9 @@ tacker vnf-delete testVNF1
 tacker vnf-delete testVNF2
 tacker vnfd-delete test-vnfd1 
 tacker vnfd-delete test-vnfd2
-openstack stack delete sfc
-openstack stack delete sfc_test1
-openstack stack delete sfc_test2
+openstack stack delete sfc --y
+openstack stack delete sfc_test1 --y
+openstack stack delete sfc_test2 --y
 " >> delete.sh
 
 chmod +x delete.sh
