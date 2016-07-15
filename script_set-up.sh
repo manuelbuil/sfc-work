d6064e344e1212504df9caf43e2fe61c3bd1428a
Manuel Buil <manuel.buil@ericsson.com>
2016-07-15 15:16:42 +0200

    Update script_set-up.sh

--- script_set-up.sh
+++ script_set-up.sh
@@ -105,7 +105,6 @@ source ~/yardstick_venv/bin/activate
 easy_install -U setuptools
 git clone https://gerrit.opnfv.org/gerrit/yardstick
 cd yardstick
-git fetch https://gerrit.opnfv.org/gerrit/yardstick refs/changes/77/14577/27 && git checkout FETCH_HEAD
 python setup.py install
 source ../tackerc
 
