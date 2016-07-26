1b6c4f4cbf4360c5aef0a1fc20f21b49845ca81f
root <root@fuel.domain.tld>
2016-07-26 11:36:52 +0000

    Added new flows
    Signed-off-by: root <root@fuel.domain.tld>

--- configure_first_classifier_ovs25.sh
+++ configure_first_classifier_ovs25.sh
@@ -1,7 +1,8 @@
 output_port=6
-nsp=0x32
+nsp=0x31
 ip=0xc0a8002b
 nsp_dec=$(($nsp))
+SF_port=4
 
 echo $nsp
 echo $ip
@@ -14,6 +15,8 @@ ovs-ofctl -O Openflow13 del-flows br-int "table=11,tcp,reg0=0x1,tp_dst=22"
 ovs-ofctl -O Openflow13 add-flow br-int "table=11,tcp,reg0=0x1,tp_dst=80 actions=move:NXM_NX_TUN_ID[0..31]->NXM_NX_NSH_C2[],push_nsh,load:0x1->NXM_NX_NSH_MDTYPE[],load:0x3->NXM_NX_NSH_NP[],load:$ip->NXM_NX_NSH_C1[],load:$nsp->NXM_NX_NSP[0..23],load:0xff->NXM_NX_NSI[],load:$ip->NXM_NX_TUN_IPV4_DST[],load:$nsp->NXM_NX_TUN_ID[0..31],resubmit($output_port,0)"
 ovs-ofctl -O Openflow13 add-flow br-int "table=11,tcp,reg0=0x1,tp_dst=22 actions=move:NXM_NX_TUN_ID[0..31]->NXM_NX_NSH_C2[],push_nsh,load:0x1->NXM_NX_NSH_MDTYPE[],load:0x3->NXM_NX_NSH_NP[],load:$ip->NXM_NX_NSH_C1[],load:$nsp->NXM_NX_NSP[0..23],load:0xff->NXM_NX_NSI[],load:$ip->NXM_NX_TUN_IPV4_DST[],load:$nsp->NXM_NX_TUN_ID[0..31],resubmit($output_port,0)"
 
+#ovs-ofctl -O Openflow13 add-flow br-int "table=11, nsi=254,nsp=$nsp_dec,in_port=6 actions=load:0x1->NXM_NX_REG0[], move:NXM_NX_NSH_C2[]->NXM_NX_TUN_ID[0..31], resubmit($SF_port,1)"
+#ovs-ofctl -O Openflow13 add-flow br-int "table=1, nsi=254,nsp=$nsp_dec,reg0=0x1,in_port=$SF_port actions=pop_nsh,goto_table:21"
 
 ovs-ofctl -O Openflow13 del-flows br-int "table=158,nsi=255,nsp=$nsp_dec"
 ovs-ofctl -O Openflow13 add-flow br-int "table=158, priority=651,nsi=255,nsp=$nsp_dec actions=move:NXM_NX_NSH_MDTYPE[]->NXM_NX_NSH_MDTYPE[],move:NXM_NX_NSH_NP[]->NXM_NX_NSH_NP[],move:NXM_NX_NSH_C1[]->NXM_NX_NSH_C1[],move:NXM_NX_NSH_C2[]->NXM_NX_NSH_C2[],move:NXM_NX_TUN_ID[0..31]->NXM_NX_TUN_ID[0..31],load:0x3->NXM_NX_TUN_GPE_NP[],output:$output_port"
