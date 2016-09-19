91c4b07cc09d77b41dc60c7752d7952d8bdf3f77
root <root@fuel.domain.tld>
2016-09-19 11:14:19 +0000

    Some changes done
    Signed-off-by: root <root@fuel.domain.tld>

--- seattle/start_demo.sh
+++ seattle/start_demo.sh
@@ -1,3 +1,6 @@
 tacker sfc-classifier-delete yellow_farbe
 tacker sfc-classifier-create --name red_http --chain red --match source_port=0,dest_port=80,protocol=6
 tacker sfc-classifier-create --name red_ssh --chain red --match source_port=0,dest_port=22,protocol=6
+compute_ip=10.20.0.11
+nsp_red=0x96
+ssh ${compute_ip} 'bash sfc-work/seattle/correct_classifier.bash ${nsp_red}'
--- seattle/swap_classify_to_chainB.sh
+++ seattle/swap_classify_to_chainB.sh
@@ -5,3 +5,8 @@ tacker sfc-classifier-create --name blue_http --chain blue --match source_port=0
 tacker sfc-classifier-create --name blue_ssh  --chain blue --match source_port=0,dest_port=22,protocol=6
 sleep 5 
 tacker sfc-classifier-list
+
+compute_ip=10.20.0.11
+nsp_blue=0x96
+ssh ${compute_ip} 'bash sfc-work/seattle/correct_classifier.bash ${nsp_blue}'
+
