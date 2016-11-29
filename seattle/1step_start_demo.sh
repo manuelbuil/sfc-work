e80538b6fbec0ea77f38ef154b8a886cbad97257
root <root@node-14.domain.tld>
2016-11-29 13:17:24 +0000

    Cambio de colores
    Signed-off-by: root <root@node-14.domain.tld>

--- seattle/1step_start_demo.sh
+++ seattle/1step_start_demo.sh
@@ -1,4 +1,4 @@
-tacker sfc-classifier-delete yellow_http
+tacker sfc-classifier-delete purple_http
 tacker sfc-classifier-create --name red_http --chain red --match source_port=0,dest_port=80,protocol=6
 tacker sfc-classifier-create --name red_ssh --chain red --match source_port=0,dest_port=22,protocol=6
 #compute_ip=10.20.0.4
